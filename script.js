dcb69a7521409e4ad77b3c123730055badf7741a
Emerson Romana Senna <133699469+Minfermis@users.noreply.github.com>
2025-07-14 00:51:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
 
 
 let active = 0 
@@ -11,37 +12,30 @@ let firstPosition = 0
 let lastPosition = items.length - 1
 
 
-nextButton.onclick = () => {
-
-
+function setSlider(){
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove('active')
-
-    active = active + 1 > lastPosition ? 0 : active + 1
-    items[active].classList.add('active')
 
     let dotsOld = indicator.querySelector ('ul li.active')
     dotsOld.classList.remove('active')
-
-    active = active + 1 > lastPosition ? 0 : active + 1
     dots[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = "0" + (active + 1)
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+    active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
+    items[active].classList.add('active')
+
+     
 }
 
 prevButton.onclick = () => {
-    // Remove o carro atual
-    let itemOld = container.querySelector('.list .item.active')
-    itemOld.classList.remove('active')
-
-    // Remove o ponto (dot) atual
-    let dotsOld = indicator.querySelector('ul li.active') // <-- Corrigido seletor!
-    dotsOld.classList.remove('active')
-
-    // Atualiza a variável "active" corretamente
+    list.style.setProperty('--calculation', 1)
     active = active - 1 < 0 ? lastPosition : active - 1
-
-    // Adiciona o novo carro e o novo ponto (dot)
+    setSlider()
     items[active].classList.add('active')
-    dots[active].classList.add('active')
 
-    indicator,querySelector('.number').innerHTML = active + 1 
 }
